107397daa673652486de7ea1ed41d6b16c9ef522
Juseong (Joe) Kim <56618654+juseong-kim@users.noreply.github.com>
2021-05-05 23:56:28 +0900

    update README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Badge from "react-bootstrap/Badge";
+let marked = require("marked");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      markdown:"",
+    };
+  }
+
+  updateMarkdown(markdown){
+    this.setState({markdown});
+  }
+
+  render(){
+    var inputStyle = {
+      width: "100%",
+      height: "100%",
+    };
+    var textBoxStyle = {
+      width: "100%",
+      height: "100vh",
+      padding:"30px",
+      resize:"none",
+      borderRadius: "3px",
+      border: "transparent",
+      outline: "none",
+      backgroundColor: "rgba(0,0,0,0.1)",
+    };
+    var outputStyle = {
+      width: "100%",
+      height: "auto",
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding:"30px"
+    };
+
+    return(
+      <div className = "App">
+        <div className = "container-fluid p-0">
+          <div className="row no-gutters">
+            <div className="col-md-6">
+              <div className="mark-input" style={inputStyle}>
+                <textarea className="input" style={textBoxStyle}
+                  value={this.state.markdown} onChange={(e)=>{this.updateMarkdown(e.target.value)}}
+                  placeholder="mark it down here and see it there -->" wrap="off"> 
+                </textarea>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div style={outputStyle}
+                    dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
-  );
+    );}
 }
-
-export default App;
